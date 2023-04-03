4ecaca6fb019754f49815e347edb1dd03075ff9c
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2023-04-04 00:08:03 +0900

    [개선] 다이얼로그 관련 한글변수 제거

---
+++ Assets/Asset/dialogue/move/interactionEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactionEvent : MonoBehaviour
+{
+    [SerializeField] DialogEvent dialog;
+
+    public Dialog[] GetDialogs()
+    {
+        dialog.dialogs = DialogDatabaseManager.instance.GetDialog((int)dialog.line.x, (int)dialog.line.y);
+
+        return dialog.dialogs;  
+    }
+}
