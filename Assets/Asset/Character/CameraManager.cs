f865e8ea47f92ce7738c8625c39c12b5e9e12746
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2023-04-08 23:20:01 +0900

    [추가] 플레이어블 캐릭터 생성, 카메라 추적 추가

---
+++ Assets/Asset/Character/CameraManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+
+
+    public GameObject target; // 카메라가 따라갈 대상
+    public float moveSpeed; //카메라 이동 속도
+
+    private Vector3 targetPosition; // 대상의 현재위치
+
+    public bool isCheckEnd = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+
+    public void 카메라다시움직이기()
+    {
+        isCheckEnd = false;
+    }
+
+    public void 카메라멈추기()
+    {
+        isCheckEnd = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "endStage")
+        {
+            isCheckEnd = true;
+        }
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target.gameObject != null)
+        {
+            if (isCheckEnd == false)
+            {
+                targetPosition.Set(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+                this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            }
+
+        }
+        
+    }
+}
---
+++ Assets/Asset/Character/Player.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+	Animator anime;
+
+	float ChInRommSize = 2.183431f;
+
+	public float runPower = 6f;
+	Rigidbody2D rigid;
+	Transform trans;
+
+
+
+
+	// Use this for initialization
+	void Start()
+	{
+		trans = gameObject.GetComponent<Transform>();
+		rigid = gameObject.GetComponent<Rigidbody2D>();
+		anime = gameObject.GetComponent<Animator>();
+
+
+	}
+
+    // Update is called once per frame
+    private void Update()
+	{
+
+		runAndWalkChanger();
+		Move();
+
+	}
+
+
+	void runAndWalkChanger()
+    {
+		if(Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0)
+		{
+			anime.SetFloat("move", 1);
+        }
+
+		else if (Input.GetAxisRaw("Horizontal") == 0)
+		{
+			anime.SetFloat("move", 0);
+		}
+	}
+
+	void Move()
+	{
+		Vector3 moveVelocity = Vector3.zero;
+
+		if (Input.GetAxisRaw("Horizontal") < 0 && Input.GetKey(KeyCode.Z))
+		{
+			moveVelocity = Vector3.left;
+			transform.localScale = new Vector3(-ChInRommSize, ChInRommSize, 1);
+			//X값 스케일을 -1로 주어 좌우반전
+		}
+		else if (Input.GetAxisRaw("Horizontal") > 0 && Input.GetKey(KeyCode.Z) )
+		{
+			moveVelocity = Vector3.right;
+			transform.localScale = new Vector3(ChInRommSize, ChInRommSize, 1);
+			//X값 스케일을 1로 주어 다시 원위치 
+		}
+
+		if (Input.GetAxisRaw("Horizontal") < 0 )
+		{
+
+			moveVelocity = Vector3.left;
+
+			transform.localScale = new Vector3(-ChInRommSize, ChInRommSize, 1);
+			//X값 스케일을 -1로 주어 좌우반전
+		}
+		else if (Input.GetAxisRaw("Horizontal") > 0)
+		{
+			moveVelocity = Vector3.right;
+
+			transform.localScale = new Vector3(ChInRommSize, ChInRommSize, 1);
+			//X값 스케일을 1로 주어 다시 원위치 
+		}
+
+
+		transform.position += moveVelocity * runPower * Time.deltaTime;
+
+	}
+
+
+
+
+
+}
